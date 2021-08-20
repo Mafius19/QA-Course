12901355c41cd19e0967879f0bd1c19fedb5c237
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-20 18:20:29 -0300

    ej 11: Pedir el ingreso de 4 números y retornar su suma en valor absoluto.

--- src/PractiseAplication.java
+++ src/PractiseAplication.java
@@ -4,23 +4,27 @@ public class PractiseAplication {
     public static void main(String args[]) {
         //Keyboard
         Scanner input = new Scanner(System.in);
-        int num1,num2,num3;
+        int num1,num2,num3,num4,suma;
 
-        //Enter the 3 numbers
+        //Enter the 4 numbers
         System.out.println("Ingrese un número");
         num1 = input.nextInt();
 
         System.out.println("Ingrese otro número");
         num2 = input.nextInt();
 
-        System.out.println("Ingrese un último número");
+        System.out.println("Ingrese otro número");
         num3 = input.nextInt();
 
+        System.out.println("Ingrese un último número");
+        num4 = input.nextInt();
+
         //Calculate total
-        if (num1 != num2 && num2 != num3 && num1 != num3) {
-            System.out.println("Todos los números son diferentes");
-        } else {
-            System.out.println("Hay números repetidos");
+        suma = num1 + num2 + num3 + num4;
+
+        if (suma < 0) {
+            suma = suma * -1;
         }
+        System.out.println("El valor absoluto de la suma es: " + suma);
     }
 }
