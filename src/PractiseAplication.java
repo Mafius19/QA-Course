228c399b16d1074bba316f48ebc9c842700ce6a3
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-20 16:10:29 -0300

    Ej 2: Pedir el ingreso del nombre y el año de nacimiento. Mostrar el nombre y la edad de la persona.

--- src/PractiseAplication.java
+++ src/PractiseAplication.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class PractiseAplication {
     public static void main(String args[]) {
-        System.out.println("Hola mundo!");
+        //Keyboard
+        Scanner input = new Scanner(System.in);
+
+        //Enter your name
+        System.out.println("Ingrese su nombre");
+        String nombre = input.next();
+
+        //Enter year of birth
+        System.out.println("Ingrese su año de nacimiento");
+        int año = input.nextInt();
+
+        //Calculate age
+        int edad = 2021 - año;
+
+        //show name and age
+        System.out.println("Su nombre es " + nombre + " y su edad es de " + edad + " años.");
     }
 
 }
