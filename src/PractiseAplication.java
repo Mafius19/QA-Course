e0d29596713f2c9158b4651d8ebf69c9ea361471
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-20 16:46:31 -0300

    ej 4: Pedir el ingreso de un dato, e indicar si es menor o mayor a 0

--- src/PractiseAplication.java
+++ src/PractiseAplication.java
@@ -4,23 +4,17 @@ public class PractiseAplication {
     public static void main(String args[]) {
         //Keyboard
         Scanner input = new Scanner(System.in);
-        int num1,num2,num3;
+        int num;
 
         //Enter the 3 numbers
         System.out.println("Ingrese un número");
-        num1 = input.nextInt();
+        num = input.nextInt();
 
-        System.out.println("Ingrese otro número");
-        num2 = input.nextInt();
+        if (num > 0) {
+            System.out.println("El número ingresado es mayor a 0");
+        } else {
+            System.out.println("El número ingresado es menor o igual a 0");
+        }
 
-        System.out.println("Ingrese un último número");
-        num3 = input.nextInt();
-
-        //Calculate total
-        int sum = num1 + num2 + num3;
-
-        //show the sum
-        System.out.println("La suma de los 3 números ingresados es: " + sum);
     }
-
 }
