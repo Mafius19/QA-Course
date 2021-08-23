ad0449132ecf5d6e0f9774a29ca6cdbc7a118444
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-23 18:13:31 -0300

    ej 24: Calcular la suma de los 10 primeros números naturales

--- src/PractiseAplication.java
+++ src/PractiseAplication.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class PractiseAplication {
     public static void main(String args[]) {
         int i;
-        for (i = 100; i >= 90; i--){
-            System.out.println(i);
+        int acc = 0;
+        for (i = 1; i <= 10; i++){
+            acc = acc + i;
         }
+        System.out.println("La suma de los 10 primeros numeros naturales es: " + acc);
     }
 }
