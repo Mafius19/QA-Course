7bf902b5953a43df1efe1e4a72a7104d152e2ea3
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-23 18:54:20 -0300

    Calcula el factorial del número 5.

--- src/PractiseAplication.java
+++ src/PractiseAplication.java
@@ -3,12 +3,10 @@ import java.util.Scanner;
 public class PractiseAplication {
     public static void main(String args[]) {
         int i;
-        int esMultiplo = 0;
-        for (i = 10; i <= 70; i++){
-            if (((double) i % 3) == 0) {
-                esMultiplo++;
-            }
+        double factorial = 1;
+        for (i = 5; i > 0; i--){
+            factorial = factorial * i;
         }
-        System.out.println("La cantidad de múltiplos de 3 que existen entre el 10 y el 70 son: " + esMultiplo);
+        System.out.println("El factorial de 5 es: " + factorial);
     }
 }
