368156751d23c146e4722567d13c6aa5f545a5d2
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-23 16:42:56 -0300

    Mostrar el resultado del examen: * Si la nota está entre 0 y 59 → Eliminado * Si la nota está entre 60 y 79 → Aprobado * Si la nota está entre 80 y 100 → Aprobado con Excelente * Si el numero esta fuera de 0 y 100, mostrar que se ha ingresado una nota invalida

--- src/PractiseAplication.java
+++ src/PractiseAplication.java
@@ -4,18 +4,22 @@ public class PractiseAplication {
     public static void main(String args[]) {
         //Keyboard
         Scanner input = new Scanner(System.in);
-        int num = 1;
-        int acc = 0;
-        int ingresados = 0;
-        double promedio;
+        int resultado;
 
-        while (num != 0){
-            System.out.println("Ingrese un número");
-            num = input.nextInt();
-            acc = acc + num;
-            ingresados++;
-            promedio = (double) acc / ingresados;
-            System.out.println("El promedio de los números ingresados es: " + promedio);
+        System.out.println("Ingrese el resultado del examen: ");
+        resultado = input.nextInt();
+
+        if (resultado >= 0 && resultado <= 59) {
+            System.out.println("En base a tu resultado: " + resultado + " estas ELIMINADO");
+        }
+        if (resultado >= 60 && resultado <= 79) {
+            System.out.println("En base a tu resultado: " + resultado + " estas APROBADO");
+        }
+        if (resultado >= 80 && resultado <= 100) {
+            System.out.println("En base a tu resultado: " + resultado + " estas APROBADO CON EXCELENTE");
+        }
+        if (resultado < 0 && resultado > 100) {
+            System.out.println("Resultado ingresado incorrecto");
         }
     }
 }
