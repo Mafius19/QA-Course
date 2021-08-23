6b9434d2084053474227f705eff8196ce1d97434
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-23 18:37:13 -0300

    Mostrar por pantalla cuántos múltiplos de 3 existen entre el 10 y el 70

--- src/PractiseAplication.java
+++ src/PractiseAplication.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class PractiseAplication {
     public static void main(String args[]) {
         int i;
-        for (i = 1; i <= 500; i=i+4){
-            System.out.println(i);
+        int esMultiplo = 0;
+        for (i = 10; i <= 70; i++){
+            if (((double) i % 3) == 0) {
+                esMultiplo++;
+            }
         }
+        System.out.println("La cantidad de múltiplos de 3 que existen entre el 10 y el 70 son: " + esMultiplo);
     }
 }
