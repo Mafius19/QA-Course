2d67184cf740b2d1c06caabe715b0f90fdecf3c5
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-23 16:07:22 -0300

    ej 14: Pedir el ingreso de números hasta que se ingrese un 0. Mostrar el promedio de ellos.

--- src/PractiseAplication.java
+++ src/PractiseAplication.java
@@ -6,12 +6,16 @@ public class PractiseAplication {
         Scanner input = new Scanner(System.in);
         int num = 1;
         int acc = 0;
+        int ingresados = 0;
+        double promedio;
 
         while (num != 0){
             System.out.println("Ingrese un número");
             num = input.nextInt();
             acc = acc + num;
-            System.out.println("La suma de los números ingresados hasta ahora es: " + acc);
+            ingresados++;
+            promedio = (double) acc / ingresados;
+            System.out.println("El promedio de los números ingresados es: " + promedio);
         }
     }
 }
