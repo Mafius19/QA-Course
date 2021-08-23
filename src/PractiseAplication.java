9cf750874ab7cf5d5bd14a3c7b792e3b404184c5
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-23 17:02:45 -0300

    ej 16: Imprimir los números del 1 al 10.

--- src/PractiseAplication.java
+++ src/PractiseAplication.java
@@ -2,24 +2,9 @@ import java.util.Scanner;
 
 public class PractiseAplication {
     public static void main(String args[]) {
-        //Keyboard
-        Scanner input = new Scanner(System.in);
-        int resultado;
-
-        System.out.println("Ingrese el resultado del examen: ");
-        resultado = input.nextInt();
-
-        if (resultado >= 0 && resultado <= 59) {
-            System.out.println("En base a tu resultado: " + resultado + " estas ELIMINADO");
-        }
-        if (resultado >= 60 && resultado <= 79) {
-            System.out.println("En base a tu resultado: " + resultado + " estas APROBADO");
-        }
-        if (resultado >= 80 && resultado <= 100) {
-            System.out.println("En base a tu resultado: " + resultado + " estas APROBADO CON EXCELENTE");
-        }
-        if (resultado < 0 && resultado > 100) {
-            System.out.println("Resultado ingresado incorrecto");
+        int i;
+        for (i = 1; i <= 10; i++){
+            System.out.println(i);
         }
     }
 }
