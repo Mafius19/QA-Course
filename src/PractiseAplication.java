bf476283735becd3229fef232f6542d04e1653de
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-23 23:48:39 -0300

    ej 27: Programa Java que muestre los números del 1 al 10 utilizando la instrucción while.

--- src/PractiseAplication.java
+++ src/PractiseAplication.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class PractiseAplication {
     public static void main(String args[]) {
-        int i;
-        double factorial = 1;
-        for (i = 5; i > 0; i--){
-            factorial = factorial * i;
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
         }
-        System.out.println("El factorial de 5 es: " + factorial);
     }
 }
