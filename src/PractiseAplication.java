f2eadb1f078b8a62784a9c6585a07cdc67379e9f
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-24 01:09:03 -0300

    Mostrar los múltiplos de 9 entre 0 y 100 que sean impares.

--- src/PractiseAplication.java
+++ src/PractiseAplication.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class PractiseAplication {
     public static void main(String args[]) {
-        int i = 1;
-        while (i <= 10) {
-            System.out.println(i);
+        double i = 0;
+        while (i <= 100) {
+            if ((i % 9 == 0) && (i % 2 != 0)) {
+                System.out.println(i);
+            }
             i++;
         }
     }
