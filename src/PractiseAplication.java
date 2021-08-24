3354db752f75f8a5a72c5cdd29aaf877f7653976
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-24 01:11:13 -0300

    ej 29: Imprimir los números del 10 al 1000, de 10 en 10.

--- src/PractiseAplication.java
+++ src/PractiseAplication.java
@@ -2,12 +2,10 @@ import java.util.Scanner;
 
 public class PractiseAplication {
     public static void main(String args[]) {
-        double i = 0;
-        while (i <= 100) {
-            if ((i % 9 == 0) && (i % 2 != 0)) {
-                System.out.println(i);
-            }
-            i++;
+        int i = 0;
+        while (i <= 1000) {
+            System.out.println(i);
+            i = i + 10;
         }
     }
 }
