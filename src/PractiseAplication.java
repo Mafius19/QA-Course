e9c301f4d1f0957c9b09d552775f49b37db70891
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-30 20:07:36 -0300

    Ejercicio 2 resuelto

--- src/PractiseAplication.java
+++ src/PractiseAplication.java
@@ -1,12 +1,23 @@
 //Practico 3
 public class PractiseAplication {
     public static void main(String args[]) {
+        /*Ejercicio 1:
         int doble = calcularDoble(5);
-        System.out.println((doble));
+        System.out.println((doble));*/
+        //Ejercicio 2:
+        compararCon20(25);
     }
     //1 - Escribir un método que reciba un número y retorne su doble
     public static int calcularDoble(int num) {
         return num*2;
     }
-
+    //2 - Método que reciba un número entero e imprima en pantalla en caso de ser mayor a 20, “Es mayor a 20”.
+    //De lo contrario, imprimir “No es mayor a 20”
+    public static void compararCon20(int num) {
+        if (num > 20) {
+            System.out.println("Es mayor a 20");
+        } else {
+            System.out.println("No es mayor a 20");
+        }
+    }
 }
